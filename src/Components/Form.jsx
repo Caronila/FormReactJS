c507a137c912a21052fd1730ac95c2c6e7ed5db9
Caronila <carolinacardoso92@gmail.com>
2023-11-16 23:46:03 -0300

    Updating labels + state
    Adding htmlFor in labels.
    Starting states for saving form data.

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Form.css'
+
+
+useState
 
 export default function Form() {
     return (
         
-            <div className='form'>
-                <form>
-                    <ul>
-                        <li><label>Nombre:</label>
-                            <input type="text" id="nombre" name="nombre" required /></li>
-                        <li><label>Apellido:</label>
-                            <input type="text" id="apellido" name="apellido" required />
-                        </li>
-                        <li><label>Email:</label>
-                            <input type="email" id="email" name="email" required />
-                        </li>
-                        <li><label>Telefono:</label>
-                            <input type="number" id="phone" name="phone" required />
-                        </li>
-                        <li> <label>Password:</label>
-                            <input type="password" id="password" name="password" required />
-                        </li>
-                        <li><label>Confirmar password:</label>
-                            <input type="password" id="password" name="password" required />
-                        </li>
-                    </ul>
-                    </form>
-                    <button type='submit'>Enviar</button>
 
-                    </div>
+        <div className='form'>
+            <form>
+                <ul>
+                    <li><label htmlFor="nombre">Nombre:</label>
+                        <input type="text" name="nombre" required /></li>
+                    <li><label htmlFor="apellido">Apellido:</label>
+                        <input type="text" name="apellido" required />
+                    </li>
+                    <li><label htmlFor="email">Email:</label>
+                        <input type="email" name="email" required />
+                    </li>
+                    <li><label htmlFor="phone">Telefono:</label>
+                        <input type="number" name="phone" required />
+                    </li>
+                    <li> <label htmlFor="password">Password:</label>
+                        <input type="password" name="password" required />
+                    </li>
+                    <li><label htmlFor="password">Confirmar password:</label>
+                        <input type="password" name="password" required />
+                    </li>
+                </ul>
+            </form>
+            <button type='submit'>Enviar</button>
 
-       
+        </div>
+
+
     )
 }
